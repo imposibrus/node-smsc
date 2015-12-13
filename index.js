d1353ef021cd64dc62d833a8b6b493df014388ff
imposibrus <imposibrus@gmail.com>
2015-12-13 20:59:26 +0300

    fix - switch to https fix - parse response body before callback fix - use JSON by default

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 /**
  * Module dependencies.
  */
-var http                = require('http');
+var https                = require('https');
 var querystring         = require("querystring");
 
 // End of dependencies.
@@ -25,19 +25,34 @@ var Sender = module.exports = function (login, passwd) {
 
 Sender.prototype.sms = function(phones, message, cb) {
   var uri = [
-    'http://www.smsc.ru/sys/send.php?login=', this.credentials.login,
+    'https://www.smsc.ru/sys/send.php?login=', this.credentials.login,
     '&psw=', this.credentials.passwd,
     '&phones=', phones,
     '&mes=', querystring.escape(message),
-    '&charset=utf-8'
+    '&charset=utf-8',
+    '&fmt=3'
   ].join('');
   this.request(uri, cb);
 };
 
 
 Sender.prototype.request = function(uri, cb) {
-  http.get(uri, function(res) {
-    cb(null, res);
+  https.get(uri, function(res) {
+    var resData = '',
+        resJSON = {};
+
+    res.on('data', function(data) {
+      resData += data;
+    });
+    res.on('end', function() {
+      try {
+        resJSON = JSON.parse(resData);
+      } catch(e) {
+        return cb(e);
+      }
+
+      cb(null, resJSON, res);
+    });
   }).on('error', function(err) {
     cb(err);
   });
